aa7aed7bdd5790a2449befb58ec94cef3ef1567e
Jessica Page <Jessica@BaSIC.local>
2020-03-20 11:57:52 -0400

    Commit working changes to remote to allow work on different desktop

--- clip_habmaps_arc.py
+++ clip_habmaps_arc.py
@@ -6,9 +6,13 @@ Created on Mon Mar 16 16:26:40 2020
 
 Description: Clips CONUS GAP habitat maps (summer) to the WV boundary.
 """
-import os, sys, fiona
-import rasterio
-import rasterio.mask
+import os, sys, arcpy, arcgisscripting
+from arcpy import env  
+from arcpy.sa import *
+gp=arcgisscripting.create(9.3)
+
+arcpy.env.overwriteOutput = 1
+
 
 # Set paths to data
 projDir = "P:/Proj6/GAP-WVBA/"
@@ -19,6 +23,13 @@ listDir = dataDir + '/Specieslists/WV_AtlasCodes.csv'
 wvBoundary = projDir + 'WV_GAPcover/2001/WVworkspace/wv_bound.shp'
 outputGDB = projDir + 'WV_GAPcover/2001/WVworkspace/Temp'  ##Temp file for spp maps to be downloaded to
 
+if arcpy.Exists(outputGDB) == False:
+    print "Creating " + outputGDB
+    arcpy.CreateFileGDB_management(os.path.dirname(outputGDB),outputGDB.split("/")[-1])
+
+# Set workspace directory for spatial processing
+arcpy.env.scratchWorkspace = projDir + 'WV_GAPcover/2001/WVworkspace'
+arcpy.env.workspace = projDir + 'WV_GAPcover/2001/WVworkspace'
 
 # Get a list of GAP species codes to process
 
@@ -39,9 +50,10 @@ for uid in uniqueIDs:
         #add repo code here, ask Nathan 
         
         #save clipped map to clipDir with spp code
-        with fiona.open("wvBoundary", "r") as shapefile:
-            shapes = [feature["geometry"] for feature in shapefile] #value raster is a placeholder for the current downloaded hab data
+        arcpy.Clip_management(valueRaster,"",clipDir + \\str(uid).tif,"wvBoundary","","ClippingGeometry", "NO_MAINTAIN_EXTENT")  #value raster is a placeholder for the current downloaded hab data
     except:																									##Print when route region group is not successfully performed
         print "Something went wrong with " + str(uid) + "."  
+
+
 
 
--- match_species.py
+++ match_species.py
@@ -1,14 +1,35 @@
 import os
 import pandas as pd
+pd.set_option('display.width', 1000)
+pd.set_option('display.max_colwidth', 400)
+pd.set_option('display.max_rows', 400)
+pd.set_option('display.max_columns', 10)
 
-atlas = 'C:\\Users\\jhpage\\Specieslists\\WVAtlasSPP.csv'
-gap = 'C:\\Users\\jhpage\\Specieslists\\Gap_birds_WV.csv'
-result = 'C:\\Users\\jhpage\\Specieslists\\Gap_birds_WV.csv'
+projDir = "P:/Proj6/GAP-WVBA/Data"
+atlas = projDir + "WVAtlasSPP.csv"
+gap = projDir + "Gap_birds_WV.csv"
+result = projDir + "Gap_birds_WV.csv"
+result2 = projDir + "Gap_birds_WV.csv"
 
-##read gap and atlas files in as dataframes
-df1 = pd.read_csv(gap)
-df2 = pd.read_csv(atlas)
+## read gap and atlas files in as dataframes
+df1 = pd.read_csv(gap, dtype={'strCommonName': 'string'})
+print(df1.head())
+df2 = pd.read_csv(atlas, dtype={'strCommonName': 'string'})
+df2['strCommonName'] = [x.rstrip() for x in df2['strCommonName']]
+
+print('\n')
+print(df2.head())
 
 ##Join atlas spp to gap spp and export to csv
-result = pd.merge(df1, df2, how='outer', on='strScientificName')
-result.to_csv('C:\\Users\\jhpage\\Specieslists\\WV_GAP_Atlas.csv', index = False, header=True)
+result = df1.merge(df2, how='outer', on='strCommonName')
+print('\n')
+print(result.head(10))
+print(len(result))
+result.to_csv(projDir + 'WV_GAP_Atlas.csv', index = False, header=True)
+
+##Join atlas spp to gap spp to include non-matches and export to csv
+result2 = pd.merge(df1, df2, how='right', on='strCommonName')
+result2.to_csv(projDir + 'WV_AtlasCodes.csv', index = False, header=True)
+print('\n')
+print(result2.head(10))
+print(len(result2))
