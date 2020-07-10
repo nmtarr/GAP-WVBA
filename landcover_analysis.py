6f70218af8e53ddea1570ed056de66d9fdd1d6c3
Nathan Tarr <nmtarr@ncsu.edu>
2020-07-10 15:55:46 -0400

    Rerun elevaiton notebook, work on land cover assessment.

--- landcover_analysis.py
+++ landcover_analysis.py
@@ -3,30 +3,74 @@
 Created on Mon Jun  1 15:08:21 2020
 
 @author: nmtarr
-"""
 
+Issues -- invalid habitat codes recorded 
+"""
 import pandas as pd
 import repo_functions as fun
-import numpy as np
-pd.set_option('display.width', 1000)
-pd.set_option('display.max_colwidth', 400)
-pd.set_option('display.max_rows', 400)
-pd.set_option('display.max_columns', 10)
 
 # Load GAP cover associations
-GAPlc0 = pd.read_csv(fun.resultsDir + "habitat_summary.csv", 
-                     header=0)
+GAP = pd.read_csv(fun.resultsDir + "GAP_spp_lc_cells.csv", header=0)
 
 # Load WV cover associations
-WVlc0 = pd.read_csv(fun.resultsDir + "bbaHab_summary.csv", 
-                     header=0) 
+WV = pd.read_csv(fun.resultsDir + "WV_spp_lc_detections.csv", header=0) 
+
+# Load WV tally of records per cover type
+WV_tally = pd.read_csv(fun.dataDir + "LandCover/WV_habitat_entries.csv",
+                            header=0)
 
 # Load land cover crosswalk
-WVlc0 = pd.read_csv(fun.resultsDir + "ary.csv", 
-                     header=0) 
+cross = pd.read_csv(fun.dataDir + "LandCover/land_cover_crosswalk.csv", 
+                    header=0) 
+
+# Which GAP cover types were sampled? HOW TO DO THIS??????????????????????????
+
+# Define a species -----------------------------------------------------------
+species = "Acadian Flycatcher"
+
+# Return associations --------------------------------------------------------
+GAP_types = fun.GAP_mapped_in(species)
+print(GAP_types)
+WVBBA_types = fun.WVBBA_detected_in(species)
+print(WVBBA_types)
+
+# which GAP associations were supported? -------------------------------------
+validated = set([])
+for code in WVBBA_types:
+    print(code)
+
+    # WVtoGAP(code):
+    GAPs = (pd.read_csv(fun.dataDir + "LandCover/land_cover_crosswalk.csv", 
+                        header=0)
+             [lambda x: x['wv_code_fine'] == code]
+             ['GAP_code']
+             .unique())
+    GAPs = [str(int(x)) for x in GAPs]
+    # return GAPs
+    validated = set(GAPs) | validated
+print(validated)
 
 
 
 
 
-# What habitat types were found in the counts, but not in lc?
+
+
+
+
+
+
+
+
+'''
+# gap to wv query ------------------------------------------------------------
+code = GAP_types[0]
+# GAPtoWV(code):
+WVs = (pd.read_csv(fun.dataDir + "LandCover/land_cover_crosswalk.csv", 
+                    header=0)
+         [lambda x: x['GAP_code'] == code]
+         ['wv_code_fine']
+         .unique())
+# return WVs
+print(WVs)
+'''
--- repo_functions.py
+++ repo_functions.py
@@ -11,6 +11,47 @@ WV_pc_site = pd.read_excel(io=dataDir + "WVBBA_DATA.xlsx", sheet_name="all data"
 WV_pc_site_spp = pd.read_excel(io=dataDir + "WVBBA_DATA.xlsx", sheet_name="HABDATA_for_newCHART")
 WV_hab_labels = pd.read_excel(io=dataDir + "WVBBA_DATA.xlsx", sheet_name="Habitat type")
 
+def GAP_spp_code(name):
+    '''
+    Return the GAP species code for the common name provided.
+    '''
+    code = (pd.read_csv(dataDir + "/SpeciesLists/WV_GAP_Atlas.csv", header=0)
+            [lambda x: x['strCommonName'] == name]
+            ['strUC']
+            .iloc[0])
+    code = code[0] + code[1:5].upper() + code[5]
+    return code
+
+def WVBBA_detected_in(species):
+    '''
+    Returns list of cover types WVBBA found species in.
+    '''
+    WV = pd.read_csv(resultsDir + "WV_spp_lc_detections.csv", header=0) 
+    spp = GAP_spp_code("Acadian Flycatcher")
+    WV_types = (WV
+                [lambda x: x['species'] == spp[1:5]]
+                .T
+                .rename(columns={1: 'detections'})
+                .iloc[1:]
+                [lambda x: x['detections'] > 0]
+                .index 
+                )
+    WV_types = list(WV_types)
+    return WV_types
+
+def GAP_mapped_in(species):
+    '''
+    Returns list of GAP land cover class codes species was mapped in.
+    Format species like "Acadian Flycatcher"
+    '''
+    spp = GAP_spp_code(species)
+    GAP = pd.read_csv(resultsDir + "GAP_spp_lc_cells.csv", header=0)
+    GAP_types = (GAP
+                 [lambda x: x['GAP_sp_code'] == spp]
+                 ['GAP_lc_code']
+                 .pipe(list))
+    return GAP_types
+
 def download_GAP_range_CONUS2001v1(gap_id, toDir):
     """
     Downloads GAP Range CONUS 2001 v1 file and returns path to the unzipped
@@ -52,5 +93,4 @@ def lc_crosswalker(systems, fro, to):
     from -- classification system of systems list
     to -- classification to crosswalk list to
     """
-    #
-    return out_systems
+    #    return out_systems
