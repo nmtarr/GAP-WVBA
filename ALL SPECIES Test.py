3ede61da65848129c1a9e4d62792589518568ba9
Jessica Page <Jessica@BaSIC.local>
2020-09-03 20:38:54 -0400

    Updates to All Species Test

--- ALL SPECIES Test.py
+++ ALL SPECIES Test.py
@@ -29,7 +29,7 @@ mastdf = pd.DataFrame(index=[ "Unusable", "Supported validation",
                              columns=["Species", "detections"])
 masteval = pd.DataFrame(index=[ "Unvalidated", "Validated", 
                               "Additions"], columns=["Species", 
-                                            {"GAP_types" : int()}])
+                                            "GAP_types"])
 allunmatch = pd.DataFrame()
 
 # Load land cover crosswalk - slow loading for some reason
@@ -66,7 +66,7 @@ for species in specieslist.index[0:]:
         
         # Aggregate results for proportion of GAP systems validated------------
         eval_df = pd.DataFrame(index=[ "Unvalidated", "Validated", 
-                                      "Additions"], columns=[int("GAP_types")])
+                                      "Additions"], columns=["GAP_types"])
         eval_df.loc['Unvalidated', "GAP_types"] = len(gap_types) - len(master_valid)
         eval_df.loc['Validated', "GAP_types"] = len(master_valid)
         eval_df.loc['Additions', "GAP_types"] = len(master_add)
@@ -85,19 +85,20 @@ for species in specieslist.index[0:]:
 print(mastdf)
 print(masteval)
 
+
 #The following: drop na does make a slight difference on final results though 
 #the main purpose was to clean excess NaN records for bird not in WVBBA        
 mastdf = mastdf.dropna()
 masteval = masteval.dropna()
+masteval = masteval.astype({'GAP_types': 'int32'})
 
-#Create pivot table summarizing the validtidity of WVBBA habitat detections
+#Create pivot table summarizing the validity of WVBBA habitat detections
 pivdf = mastdf.pivot_table(index = mastdf.index,
                            aggfunc = {'detections' : [np.mean, sum, lambda x: len(x.unique())]},
                            fill_value = 0).sort_index()
 print(pivdf) 
 
 #Create pivot table summarizing the proportion of GAP systems validated 
-#(SUMMARY OF GAP_types NOT WORK DUE TO NOT INTEGER DATATYPE)
 piveval = masteval.pivot_table(index = masteval.index,
                            aggfunc = {'GAP_types' : [np.mean, sum, lambda x: len(x.unique())]},
                            fill_value = 0).sort_index()
@@ -108,11 +109,17 @@ plt1 = pivdf.drop(['Total detections']).plot(y=('detections',        'sum'),
                                              legend=False, 
                                              title = "Total Species Detections",
                                              colors=['gray', 'g', 'y'])
-plt1.set_ylabel("")   
+plt1.set_ylabel("")
+plt2 = pivdf.drop(['Total detections']).plot(y=('detections',        'mean'), 
+                                             kind='barh', legend=False, 
+                    title = "Average Species Detections")   
 
-print(eval_df)
-plt2 = piveval.plot(y='GAP_types', kind='barh', legend=False, 
-                    title = "All species GAP associations")    
+print(piveval)
+plt3 = piveval.plot(y=('GAP_types',        'sum'), kind='barh', legend=False, 
+                    title = "Total species GAP associations")    
+plt4 = piveval.plot(y=('GAP_types',        'mean'), kind='barh', legend=False, 
+                    title = "Average species GAP associations") 
+
 # In[17]:
     
 #Something is wrong with the following now, returns an empty df, see above loop 
